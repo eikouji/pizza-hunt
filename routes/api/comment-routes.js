28d4589b63c028bc683770e0a356660a0475b8e2
Eiko Ujifusa <eikorouji@gmail.com>
2022-04-12 16:50:51 -0700

    fixed comment-routes.js

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -15,17 +15,16 @@ const {
   // /api/comments/<pizzaId>
   router.route('/:pizzaId').post(addComment);
   
-  // /api/comments/<pizzaId>/<commentId>
-  router
-    .route('/:pizzaId/:commentId')
-    .put(addReply)
-    .delete(removeComment);
-  
-  // /api/comments/<pizzaId>/<commentId>/<replyId>
-  router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
-  
-  module.exports = router;
-  omments/<pizzaId>/<commentId>
-router.route('/:pizzaId/:commentId').delete(removeComment);
+  // /api/comments/<pizzaId>
+router.route('/:pizzaId').post(addComment);
+
+// /api/comments/<pizzaId>/<commentId>
+router
+  .route('/:pizzaId/:commentId')
+  .put(addReply)
+  .delete(removeComment);
+
+// /api/comments/<pizzaId>/<commentId>/<replyId>
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 
 module.exports = router;
